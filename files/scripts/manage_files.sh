49e7756b9507fb35a546c34c5d20fdd3562b97cd
Nathan Dorsey <421420+glycerine102@users.noreply.github.com>
2025-11-13 12:41:27 -0500

    fix: replaces the config file reference for netbird

--- files/scripts/manage_files.sh
+++ files/scripts/manage_files.sh
@@ -12,4 +12,7 @@ log "Ensure directories exist"
 mkdir -p /var/lib/alternatives
 mkdir -p /var/log/netbird
 
+log "Use different config file for netbird"
+sed -i 's/config\.json/default\.json/g' /usr/lib/systemd/system/netbird.service
+
 log "Done managing files"
