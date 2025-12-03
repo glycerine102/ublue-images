7443b031aa9f94bb7a196dab1b1e1bd29c55780c
Nathan Dorsey <421420+glycerine102@users.noreply.github.com>
2025-12-03 15:49:31 -0500

    feat: use zfs on desktop tasks and ensure modules are loaded

--- files/scripts/install_zfs.sh
+++ files/scripts/install_zfs.sh
@@ -5,3 +5,7 @@ set -ouex pipefail
 dnf5 install -y https://zfsonlinux.org/fedora/zfs-release-3-0$(rpm --eval "%{dist}").noarch.rpm
 dnf5 install -y kernel-devel-"$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 dnf5 --disablerepo=zfs --enablerepo=zfs-2.3 install -y zfs
+
+depmod -a "$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')" && \
+echo "zfs" > /etc/modules-load.d/zfs.conf && \
+echo "zfs" > /etc/dnf/protected.d/zfs.conf
