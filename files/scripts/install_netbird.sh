7bddfdf8b0c3fb71c40fc628a1ccafdb644d006f
Nathan Dorsey <421420+glycerine102@users.noreply.github.com>
2026-03-20 16:45:38 -0400

    feat: finish setting up a better method for installing netbird

---
+++ files/scripts/install_netbird.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -oue pipefail
+
+trap '[[ $BASH_COMMAND != echo* ]] && [[ $BASH_COMMAND != log* ]] && echo "+ $BASH_COMMAND"' DEBUG
+
+log() {
+  echo "=== $* ==="
+}
+
+log "Install netbird package"
+
+NETBIRD_VERSION="0.66.4"
+
+rpm -ivh --noscripts https://github.com/netbirdio/netbird/releases/download/v${NETBIRD_VERSION}/netbird_${NETBIRD_VERSION}_linux_amd64.rpm
+rpm -ivh --noscripts https://github.com/netbirdio/netbird/releases/download/v${NETBIRD_VERSION}/netbird-ui_${NETBIRD_VERSION}_linux_amd64.rpm
+
+log "Done installing packages"
