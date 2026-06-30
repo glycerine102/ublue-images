a7ffbe5d87eaf6a12f97eb128ac5ec9a9ba751b9
Nathan Dorsey <421420+glycerine102@users.noreply.github.com>
2026-06-30 10:20:55 -0400

    fix: don't install the netbird gui anymore

--- files/scripts/install_netbird.sh
+++ files/scripts/install_netbird.sh
@@ -13,7 +13,6 @@ log "Install netbird package"
 NETBIRD_VERSION="0.66.4"
 
 rpm -ivh --noscripts https://github.com/netbirdio/netbird/releases/download/v${NETBIRD_VERSION}/netbird_${NETBIRD_VERSION}_linux_amd64.rpm
-rpm -ivh --noscripts https://github.com/netbirdio/netbird/releases/download/v${NETBIRD_VERSION}/netbird-ui_${NETBIRD_VERSION}_linux_amd64.rpm
 
 log "Done installing packages"
 
